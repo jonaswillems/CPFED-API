859603a62552c37b958722744c0fc9feceb3cfeb
jonaswillems <r0784057@student.thomasmore.be>
2022-11-20 21:25:19 +0100

    api v1.0

---
+++ models.py
@@ -0,0 +1,17 @@
+#Created by Jonas Willems.
+#!/usr/bin/env python3
+
+
+from database import Base
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import relationship
+
+class API(Base):
+    __tablename__ = "info"
+    Id = Column(Integer, primary_key=True, index=True)
+    storeName = Column(String, index=True)
+    drinkName = Column(String, index=True)
+    drinkPrice = Column(Float, index=True)
+    
+
+
